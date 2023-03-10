dd94d82d0c58cc610cdd6f745ff58ee0ba915a60
Drew Johnson <drew.johnson@jupiterone.com>
2023-03-10 11:01:12 -0500

    Adjust image crop

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -49,7 +49,7 @@ export const About = () => {
                 width={300}
                 height={300}
                 alt={title}
-                className="object-fit w-full h-64 rounded-md shadow-2xl"
+                className="object-cover w-full h-64 rounded-md shadow-2xl"
               />
               <h2 className="font-semibold mt-4">{title}</h2>
               <ul className="mt-1 mr-2 text-gray-600 list-disc list-inside">
